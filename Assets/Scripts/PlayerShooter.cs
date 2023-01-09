d9b29393903041d3775a4c27b107a4e9f64c2e42
minu1117 <108320084+minu1117@users.noreply.github.com>
2023-01-09 22:23:14 +0900

    카메라 이동 완료

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class PlayerShooter : MonoBehaviour
 {
+    public GameObject shotPos;
+    private PlayerInput playerInput;
+    public float shotDelay = 0.1f;
+    public float range = 20f;
+    public float lastFireTime;
+
     void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
     }
 
     void Update()
+    {   
+        if (playerInput.fire && Time.time - lastFireTime > shotDelay)
+            Shot();
+    }
+
+    void Shot()
     {
-        
+        lastFireTime = Time.time;
+
+        RaycastHit hit;
+        var ray = new Ray(shotPos.transform.position, shotPos.transform.forward); // ¹ÝÁ÷¼±
+        var hitPos = Vector3.zero;
+
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            hitPos = hit.point;
+            
+        }
+        else
+        {
+            
+        }
     }
 }
